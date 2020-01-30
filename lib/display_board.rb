abde011b71495dc8a6bfe962081bcb99baea1d61
balanced-widget-4002 <jessikim807@gmail.com>
2020-01-30 03:23:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,3 +7,13 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+board = [" "," "," "," ","X"," "," "," "," "]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
